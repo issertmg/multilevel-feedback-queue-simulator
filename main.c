07783e5f2c612b16d84dd5faec62d8800deff7eb
issertmg <issertmg@gmail.com>
2021-04-20 21:22:18 +0800

    C file

---
+++ main.c
@@ -0,0 +1,14 @@
+/*************************************************************** 
+Names: GAGAN, Isser Troy M.
+       MATIAS, Maria Angela Mikaela E.
+Group: 40
+Section: S15
+***************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  
+  return 0;
+}
