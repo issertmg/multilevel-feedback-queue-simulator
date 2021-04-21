74e4172403df55f24e6042f109c29c73ff25c364
issertmg <issertmg@gmail.com>
2021-04-21 16:49:09 +0800

    added struct template for queues and processes

--- main.c
+++ main.c
@@ -8,6 +8,41 @@ Section: S15
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Process {
+  /* input*/
+  int id;                     //F
+  int arrival_time;           //G
+  int total_execution_time;   //H
+  int io_burst_time;          //I
+  int io_frequency;           //J
+  
+  /* addtnl attributes */
+  int execution_time_left;    //time left from total_execution_time
+  int io_burst_timer;         //to check if process will withhold cpu already
+
+  /* output */
+  int start_end_array_size;   //initialized to 0 (length of start_time and end_time array)
+  int start_time[500];
+  int end_time[500];
+  char queue_id [500][10]; 
+  int waiting_time;
+  int turnaround_time;
+} process;
+
+typedef struct Node {
+  process *p;
+  struct Node *next;
+} node;
+
+typedef struct Queue {
+  int id;                     //A
+  int priority;               //B
+  int time_quantum;           //C
+
+  node *front;
+  node *rear;
+} queue;
+
 int main(void) {
   
   return 0;
